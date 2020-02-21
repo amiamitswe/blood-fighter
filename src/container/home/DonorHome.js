73cfff9790f7a6a3b38dc72a6b5b9b2354d87920
amiamitswe <amiamitswe@gmail.com>
2020-02-22 00:20:35 +0600

    littel fix

--- src/container/home/DonorHome.js
+++ src/container/home/DonorHome.js
@@ -1,93 +1,100 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
-import SingalDonor from '../../components/SingalDonor';
-import Input from '../../components/Input';
-import Select from 'react-select';
+import SingalDonor from "../../components/SingalDonor";
+import Input from "../../components/Input";
+import Select from "react-select";
 
+class Home extends Component {
+  state = {
+    donor: [
+      {
+        id: 1,
+        img: "Img",
+        name: "Amit Samadder",
+        age: "24",
+        bloodGroup: "O+",
+        lastDate: "10/10/2019",
+        canDonote: "yes"
+      },
+      {
+        id: 2,
+        img: "Img",
+        name: "Amit Samadder (Abir)",
+        age: "23",
+        bloodGroup: "O+",
+        lastDate: "25/12/2019",
+        canDonote: "No"
+      },
+      {
+        id: 3,
+        img: "Img",
+        name: "Amit Samadder",
+        age: "24",
+        bloodGroup: "O+",
+        lastDate: "10/10/2019",
+        canDonote: "yes"
+      },
+      {
+        id: 4,
+        img: "Img",
+        name: "Amit Samadder",
+        age: "24",
+        bloodGroup: "O+",
+        lastDate: "10/10/2019",
+        canDonote: "yes"
+      },
+      {
+        id: 5,
+        img: "Img",
+        name: "Amit Samadder (Abir)",
+        age: "23",
+        bloodGroup: "O+",
+        lastDate: "25/12/2019",
+        canDonote: "No"
+      },
+      {
+        id: 6,
+        img: "Img",
+        name: "Amit Samadder",
+        age: "24",
+        bloodGroup: "O+",
+        lastDate: "10/10/2019",
+        canDonote: "yes"
+      }
+    ],
+    selectedOption: null
+  };
 
-  class Home extends Component {
+  handleChange = selectedOption => {
+    this.setState({ selectedOption });
+    console.log(`Option selected:`, selectedOption);
+  };
 
-    state = {
-      donor: [
-        {
-          id: 1,
-          img: "Img",
-          name: "Amit Samadder",
-          age: "24",
-          bloodGroup: "O+",
-          lastDate: "10/10/2019",
-          canDonote: "yes"
-        },
-        {
-          id: 2,
-          img: "Img",
-          name: "Amit Samadder (Abir)",
-          age: "23",
-          bloodGroup: "O+",
-          lastDate: "25/12/2019",
-          canDonote: "No"
-        },
-        {
-          id: 3,
-          img: "Img",
-          name: "Amit Samadder",
-          age: "24",
-          bloodGroup: "O+",
-          lastDate: "10/10/2019",
-          canDonote: "yes"
-        },
-        {
-          id: 4,
-          img: "Img",
-          name: "Amit Samadder",
-          age: "24",
-          bloodGroup: "O+",
-          lastDate: "10/10/2019",
-          canDonote: "yes"
-        },
-        {
-          id: 5,
-          img: "Img",
-          name: "Amit Samadder (Abir)",
-          age: "23",
-          bloodGroup: "O+",
-          lastDate: "25/12/2019",
-          canDonote: "No"
-        },
-        {
-          id: 6,
-          img: "Img",
-          name: "Amit Samadder",
-          age: "24",
-          bloodGroup: "O+",
-          lastDate: "10/10/2019",
-          canDonote: "yes"
-        }
-      ],
-      selectedOption: null
-    };
-  
-    handleChange = selectedOption => {
-      this.setState({ selectedOption });
-      console.log(`Option selected:`, selectedOption);
-    };
-
- options = [
-    { value: 'A+', label: 'A+' },
-    { value: 'A-', label: 'A-' },
-    { value: 'B+', label: 'B+' },
-    { value: 'B-', label: 'B-' },
-    { value: 'AB+', label: 'AB+' },
-    { value: 'AB-', label: 'AB-' },
-    { value: 'O+', label: 'O+' },
-    { value: 'O-', label: 'O-' }
+  bloodGroup = [
+    { value: "A+", label: "A+" },
+    { value: "A-", label: "A-" },
+    { value: "B+", label: "B+" },
+    { value: "B-", label: "B-" },
+    { value: "AB+", label: "AB+" },
+    { value: "AB-", label: "AB-" },
+    { value: "O+", label: "O+" },
+    { value: "O-", label: "O-" }
   ];
-  
+
+  locations = [
+    { value: "Barisal", label: "Barisal" },
+    { value: "Chittagong", label: "Chittagong" },
+    { value: "Dhaka", label: "Dhaka" },
+    { value: "Khulna", label: "Khulna" },
+    { value: "Mymensingh", label: "Mymensingh" },
+    { value: "Rajshahi", label: "Rajshahi" },
+    { value: "Rangpur", label: "Rangpur" },
+    { value: "Sylhet", label: "Sylhet" }
+  ];
+
   render() {
     return (
-
       <div className="home">
-  
         <div className="donor__list">
           <table>
             <thead>
@@ -96,16 +103,15 @@ import Select from 'react-select';
                 <th>Image</th>
                 <th>Name</th>
                 <th>Age</th>
-                <th>Blood Group</th>
+                <th>Blood +/-</th>
                 <th>Last Donate</th>
                 <th>Is capable</th>
                 <th>Profile</th>
               </tr>
             </thead>
-  
-            <tbody >
-  
-              {this.state.donor.map((a) =>
+
+            <tbody>
+              {this.state.donor.map(a => (
                 <SingalDonor
                   key={a.id}
                   id={a.id}
@@ -116,38 +122,35 @@ import Select from 'react-select';
                   isCapableToDonote={a.canDonote}
                   donorID={a.id}
                 />
-              )}
-  
+              ))}
             </tbody>
           </table>
         </div>
-  
+
         <div className="donor__filter">
           <Input type="number" place="Mobile No" />
-  
+
           <Select
             classNamePrefix="select__input"
             className="select__input"
             value={this.selectedOption}
             onChange={this.handleChange}
-            options={this.options}
+            options={this.bloodGroup}
             placeholder="Select Blood Group"
           />
-  
-  
+
           <Select
             classNamePrefix="select__input"
             className="select__input"
             value={this.selectedOption}
             // onChange={props.handleChange}
-            options={this.options}
+            options={this.locations}
             placeholder="Select Locaion"
           />
-  
         </div>
       </div>
     );
   }
-};
+}
 
 export default Home;
